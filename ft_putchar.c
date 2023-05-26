9c6dcf8c77413961a0aa74193a3dff2be79ef5ab
Luiza Freire <lufee20@gmail.com>
2023-05-26 17:21:13 -0300

    thoughts about it

---
+++ ft_putchar.c
@@ -0,0 +1,20 @@
+/**eu ACHO que esse deve ser 
+ * SÓ colocar o caracter?
+ * não sei 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#include <stdio.h>
+
+int main () {
+    
+    void ft_putchar(char z);
+    
+    write(1, &z, 1);
+
+    return(0);
+}
---
+++ main.c
@@ -0,0 +1,6 @@
+
+int     main()
+{
+  rush();
+  return();  
+}
