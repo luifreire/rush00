f1d97b23c44315e310c54352789d3452a2441f35
Luiza Freire <lufee20@gmail.com>
2023-05-27 11:14:27 -0300

    Update rush00.c

--- rush00.c
+++ rush00.c
@@ -1,7 +1,7 @@
 /** tem que fazer aparecer um RETÂNGULO MANO  
  * um retângulo em que o x é a largura 
  * e o y é a altura 
- * não pode crashar nem ficar de loop
+ * não pode crashar nem ficar de loop INDEFINIDO!
  *
  * função com 2 variáveis do tipo int, nomes x e y 
  */
